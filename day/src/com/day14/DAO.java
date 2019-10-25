c172b0fd88ffb40ba8186a9b0b9474aad259039f
wfwf-wq <564950414@qq.com>
2019-10-25 16:29:37 +0800

    删除了day

--- day/src/com/day14/DAO.java
+++
@@ -1,7 +0,0 @@
-package com.day14;
-//database access object
-public class DAO<T> {
-    public void add(T t){
-
-    }
-}
--- day/src/com/day14/MyAnnotation.java
+++
@@ -1,9 +0,0 @@
-package com.day14;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(value = {ElementType.METHOD})
-public @interface MyAnnotation {
-    String value() default "hello";
-}
